0b314144d747b73d44a60c9b559d35ded9e73281
rachelw106 <rachelw106@nycstudents.net>
2024-09-11 18:55:19 +0000

    commit

---
+++ datatypes.java
@@ -0,0 +1,13 @@
+public class datatypes
+{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        double b = 2.5;
+        b = a + b;
+        boolean c = true;
+        String d = "Hello";
+        String e = b + d + c;
+        System.out.print(e);
+    }
+}
